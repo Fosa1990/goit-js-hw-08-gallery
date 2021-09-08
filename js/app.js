fd6ea9ad0c4480d9d86b6debefe2ac5df91d4e16
Fosa1990 <fosa1990@gmail.com>
2021-09-08 22:38:18 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -70,52 +70,87 @@ const refs = {
   modalImage: document.querySelector('img.lightbox__image'),
 }
 
-
 refs.gallery.insertAdjacentHTML('afterbegin', createGalleryItem(galleryItems))
 
 refs.gallery.addEventListener('click', onClickGalleryitem)
+
 refs.modal.addEventListener('click', e => {
-  if (
-    e.target.classList.contains('lightbox__overlay') ||
+  if (e.target.dataset.action === 'prev-image') onPrevImage();
+  if (e.target.dataset.action === 'next-image') onNextImage();
+  if (e.target.classList.contains('lightbox__overlay') ||
     e.target.dataset.action === 'close-lightbox')
-    closeModal()
+    onCloseModal();
 }
 )
 
 function createGalleryItem(items) {
   return items
-    .map((element) =>
+    .map(({preview, original, description}) =>
   `<li class="gallery__item">
-    <a class="gallery__link" href="${element.preview}">
+    <a class="gallery__link" href="${preview}">
     <img
       class="gallery__image"
-      src="${element.preview}"
-      data-source="${element.original}"
-      alt="${element.description}"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
     />
   </a>
 </li>`)
     .join('')
 }
 
-
 function onClickGalleryitem(e) {
   if (!e.target.classList.contains('gallery__image')) return;
   e.preventDefault();
   refs.modal.classList.add('is-open');
-  setModalImgSrcAndAlt(e.target.dataset.source, e.target.alt);
+  setModalImageSrcAndAlt(e.target.dataset.source, e.target.alt);
+  window.addEventListener('keydown', onKeyDown);
 }
 
-function setModalImgSrcAndAlt(src, alt) {
+function setModalImageSrcAndAlt(src, alt) {
   refs.modalImage.src = src;
   refs.modalImage.alt = alt;
 }
 
-function closeModal(e) {
+function onCloseModal() {
   refs.modal.classList.remove('is-open');
+  setModalImageSrcAndAlt('','')
+  window.removeEventListener('keydown', onKeyDown);
 }
 
-function OnKeyDown(e) {
-  if (!refs.modal.classList.contains('is-open')) return
-  e.code = "Escape"
+function onKeyDown(e) {
+  if (!refs.modal.classList.contains('is-open')) return;
+  switch (e.code) {
+    case 'Escape':
+      onCloseModal();
+      break;
+    case 'ArrowLeft':
+      onPrevImage();
+      break;
+    case 'ArrowRight':
+      onNextImage();
+      break;
+  }
+}
+
+function onNextImage() {
+  let currentImageIndex = findImageIndex(refs.modalImage.getAttribute('src'))
+  if (currentImageIndex === galleryItems.length - 1) currentImageIndex = -1;
+  setModalImageSrcAndAlt(
+    galleryItems[currentImageIndex + 1].original,
+    galleryItems[currentImageIndex + 1].description,
+  )
+ }
+
+function onPrevImage() {
+  let currentImageIndex = findImageIndex(refs.modalImage.getAttribute('src'))
+  if (currentImageIndex === 0) currentImageIndex = galleryItems.length
+  setModalImageSrcAndAlt(
+  galleryItems[currentImageIndex - 1].original,
+  galleryItems[currentImageIndex - 1].description,
+  )
+}
+
+function findImageIndex(src) {
+ return galleryItems.indexOf(galleryItems.find(el => el.original === src))
 }
